f60aea0dd78d384242b8d15e5018713ff0e8f877
Meet <meetdomadia789@gmail.com>
2025-07-26 17:51:06 +0530

    navbar changes add some effect

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
                   </NavLink>
                   <button
                     onClick={handleLogout}
-                    className="bg-red-500 text-white font-bold py-2 px-4 rounded-md hover:bg-red-600 transition duration-300 text-sm"
+                    className="bg-red-500 cursor-pointer text-white font-bold py-2 px-4 rounded-md hover:bg-red-600 transition duration-300 text-sm"
                   >
                     Logout
                   </button>
